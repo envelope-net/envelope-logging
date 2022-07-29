8965d399060767bb8400692ba727655338d5f9b8
Tomas Pilat <tomas.pilat@disig.sk>
2022-07-29 15:50:23 +0200

    added AppendDetail to LogMessageBuilder

--- src/Envelope.Logging/ILogMessage.cs
+++ src/Envelope.Logging/ILogMessage.cs
@@ -26,7 +26,7 @@ public interface ILogMessage : Serializer.IDictionaryObject
 #elif NET6_0_OR_GREATER
 	[System.Text.Json.Serialization.JsonIgnore]
 #endif
-	Exception? Exception { get; internal set; }
+	Exception? Exception { get; set; }
 
 	string? StackTrace { get; set; }
 
